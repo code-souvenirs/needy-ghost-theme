4705cfa35587407ef7beba21ef462b55d718a0c7
Jaspalsinh Chauhan <jachauhan@gmail.com>
2014-04-30 00:29:44 +0530

    fix code issue, added top return button

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,6 +32,22 @@
 
         /* # Show loader untill everything's loaded. */
         $('.loader').show();
+
+        var offset = 220;
+        var duration = 500;
+        jQuery(window).scroll(function() {
+            if (jQuery(this).scrollTop() > offset) {
+                jQuery('.back-to-top').fadeIn(duration);
+            } else {
+                jQuery('.back-to-top').fadeOut(duration);
+            }
+        });
+        
+        jQuery('.back-to-top').click(function(event) {
+            event.preventDefault();
+            jQuery('html, body').animate({scrollTop: 0}, duration);
+            return false;
+        });
     });
 
     $(window).load(function(){
