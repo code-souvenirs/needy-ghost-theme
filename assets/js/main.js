e4af45df71e1e09f1eaaf637c7ff9635788ba7b9
Jaspalsinh Chauhan <jachauhan@gmail.com>
2015-04-01 03:39:35 +0530

    almost bootstrapped wireframe
    still one issue on xs and sm to resolve. almost ready for index, post,
    page, tag, author partials / parts

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,91 @@
 /*******************************************************************************
 	@Author			Jaspalsinh Chauhan (Jsinh)
 	@Website		http://www.jsinh.in
-	@Last Update	12:10 AM Wednesday, March 25, 2015
+	@Last Update	02:12 AM Wednesday, April 01, 2015
 
 	TABLE CONTENTS
 	-------------------------------
-    01. Variables
-		02. Globals
-		03. Code blocks
-    04. Hyperlink common styles
-		05. Back to top
-    06. Loader
-    07. Pagination
-    08. Header
-    09. Main content
-    10. Sidebar
-    11. Disclaimer
-    12. Footer
-    13. Imports
+    01. Init - Document ready - jQuery
+		02. Needy-Ghost main function call
+		03. Apply loader GIF
+    04. Apply Fit-Vids responsivness
+		05. Apply Back-to-top link on scroll
 
 *******************************************************************************/
 
-/* Init */
+/* 01. Init - Document ready - jQuery
+*******************************************************************************/
 jQuery(window).ready(function () {
 	NeedyGhost();
 });
 
+/* 02. Needy-Ghost main function call
+*******************************************************************************/
 function NeedyGhost() {
+	ApplyLoader();
+	ApplyFitVids();
+	ApplyBackToTop();
+}
 
-  /* Apply FIT-VIDS responsiveness */
+/* 03. Apply loader GIF
+*******************************************************************************/
+function ApplyLoader() {
+	/* # Show loader untill everything's loaded. */
+	$('.loader').show();
+
+	$(window).load(function() {
+			/* # All loaded, hide loader. */
+			$('.loader').hide();
+		});
+}
+
+/* 04. Apply Fit-Vids responsivness
+*******************************************************************************/
+function ApplyFitVids() {
+	/* # Apply FIT-VIDS responsiveness */
   $(".post-content").fitVids();
 }
 
+/* 05. Apply Back-to-top link on scroll
+*******************************************************************************/
+function ApplyBackToTop() {
+	var offset = 220;
+	var duration = 500;
+	jQuery(window).scroll(function() {
+			if (jQuery(this).scrollTop() > offset) {
+					jQuery('.back-to-top').fadeIn(duration);
+			} else {
+					jQuery('.back-to-top').fadeOut(duration);
+			}
+	});
+
+	jQuery('.back-to-top').click(function(event) {
+			event.preventDefault();
+			jQuery('html, body').animate({scrollTop: 0}, duration);
+			return false;
+	});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 (function ($) {
     "use strict";
 
     $(document).ready(function(){
-
-        $(".post-content").fitVids();
-
+*/
         /* Make specific links open in a new window in a HTML5 valid way */
-        $('a[rel*="external"]').click(function(){
+        /*$('a[rel*="external"]').click(function(){
           $(this).attr('target', '_blank');
         });
 
@@ -55,39 +100,17 @@ function NeedyGhost() {
                     $(this).removeClass('full-img');
                 }
             });
-        }
+        }*/
 
-        casperFullImg();
-        $(window).smartresize(casperFullImg);
+        /*casperFullImg();
+        $(window).smartresize(casperFullImg);*/
 
-        /* # Show loader untill everything's loaded. */
-        $('.loader').show();
 
-        var offset = 220;
-        var duration = 500;
-        jQuery(window).scroll(function() {
-            if (jQuery(this).scrollTop() > offset) {
-                jQuery('.back-to-top').fadeIn(duration);
-            } else {
-                jQuery('.back-to-top').fadeOut(duration);
-            }
-        });
-
-        jQuery('.back-to-top').click(function(event) {
-            event.preventDefault();
-            jQuery('html, body').animate({scrollTop: 0}, duration);
-            return false;
-        });
-    });
-
-    $(window).load(function(){
-
-      /* # All loaded, hide loader. */
-      $('.loader').hide();
-    });
-
-}(jQuery));
-
+/*
+    });*/
+/*
+}(jQuery));*/
+/*
 (function($,sr){
   // debouncing function from John Hann
   // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
@@ -113,4 +136,4 @@ function NeedyGhost() {
   // smartresize
   jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
 
-})(jQuery,'smartresize');
+})(jQuery,'smartresize');*/
