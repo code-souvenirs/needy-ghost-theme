aa0fc97540aeaf04fee98150d412e477ab4ca7dc
Jaspalsinh Chauhan <jachauhan@gmail.com>
2015-05-12 04:25:36 +0530

    Testing first draft release for version 1.2
    Experimental release to fix code formatting quickly, indentify more bugs
    to fix and mature with more HBS to add and merge to main and make final
    release

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -11,6 +11,8 @@
 		04. Apply pre-loader animation
 		05. Apply Back-to-top link on scroll
 		06. Apply sidebar pinning
+		07. Apply materialze animation
+		08. Apply post content transformation
 
 
 *******************************************************************************/
@@ -30,9 +32,8 @@ function NeedyGhost() {
 	ApplyCollapsableNavigation();
 	ApplyLoader();
 	ApplyBackToTop();
-	ApplySidebarPinning();
-	Materialize.fadeInImage('.content-wrapper');
-	Materialize.fadeInImage('.sidebar');
+	ApplyMaterializeAnimation();
+	ApplyPostContentTransformation();
 
 }
 
@@ -81,15 +82,74 @@ function ApplyBackToTop() {
 
 }
 
-/* 06. Apply Back-to-top link on scroll
-*******************************************************************************/
-function ApplySidebarPinning() {
+function ApplyMaterializeAnimation() {
 
-    if ($('header').length) {
-      $('.sidebar').pushpin({ top: $('header').height() });
-    }
-    else {
-      $('.sidebar').pushpin({ top: 0 });
-    }
+	Materialize.fadeInImage('.content-wrapper');
+	Materialize.fadeInImage('.sidebar');
 
 }
+
+function ApplyPostContentTransformation() {
+
+	$(".post-content").fitVids();
+
+	$( ".post-content img" ).wrap(function() {
+		return "<a class='fancybox-images' href='" + $(this).attr("src") + "' data-fancybox-group='defaultgallery' title='" + $(this).attr("alt") + "'>"  + $(this).text() + "</a>";
+	});
+
+	$('.fancybox-images').fancybox();
+
+  /* Make specific links open in a new window in a HTML5 valid way */
+  $('a[rel*="external"]').click(function(){
+    $(this).attr('target', '_blank');
+  });
+
+  function casperFullImg() {
+      $("img").each( function() {
+          var contentWidth = $(".post-content").outerWidth(); // Width of the content
+          var imageWidth = $(this)[0].naturalWidth; // Original image resolution
+
+          if (imageWidth >= contentWidth) {
+              $(this).addClass('full-img');
+          } else {
+              $(this).removeClass('full-img');
+          }
+      });
+  }
+
+  casperFullImg();
+  $(window).smartresize(casperFullImg);
+
+	/** Fitvids [Youtube|Vimeo] **/
+	if(jQuery(".fullwidthbanner iframe").length < 1 && jQuery(".fullscreenbanner iframe").length < 1 && jQuery(".fullscreenvideo").length < 1) { // disable fitvids if revolution slider video is present!
+		jQuery("body").fitVids();
+	}
+
+}
+
+(function($,sr){
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+      var timeout;
+
+      return function debounced () {
+          var obj = this, args = arguments;
+          function delayed () {
+              if (!execAsap)
+                  func.apply(obj, args);
+              timeout = null;
+          }
+
+          if (timeout)
+              clearTimeout(timeout);
+          else if (execAsap)
+              func.apply(obj, args);
+
+          timeout = setTimeout(delayed, threshold || 100);
+      };
+  };
+  // smartresize
+  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+})(jQuery,'smartresize');
